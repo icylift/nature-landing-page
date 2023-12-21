23fc785266c76456623f346ddf463362dfe669ff
Aaron Williams <polarizedcuriosity@gmail.com>
2023-12-20 19:42:35 -0800

    working on a sticky navbar for the whole landing page.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,27 @@
-// ----------  join btn
+// ------------------------------------  join btn   ----------------------------------------------------//
 
 const joinUsBtn = document.getElementById("joinUsBtn");
 
 function emailPage() {
   return window.open("form.html", "_self");
+}
+
+// ------------------------------------  Sticky Navbar   ----------------------------------------------------//
+
+window.onscroll = function () {
+  myFunction();
+};
+
+var navbar = document.getElementById("nav");
+
+const position = document.navbar.scrollTop;
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.position >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
